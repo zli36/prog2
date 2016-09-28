ceb675b3c5a6f4dc43bb8765c53bd732ffd18cee
Benjamin Watson <watsonb@gmail.com>
2016-09-28 06:36:03 -0400

    Create drawstuff.js

---
+++ drawstuff.js
@@ -0,0 +1,9 @@
+/* main -- here is where execution begins after window load */
+
+function main() {
+
+    // Get the canvas and context
+    var canvas = document.getElementById("viewport"); 
+    var context = canvas.getContext("2d");
+ 
+}
